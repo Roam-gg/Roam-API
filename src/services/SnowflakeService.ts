b2479ac4f8688e683afdb8f624a2b789b43dae10
Yui Yukihira <yuiyukihira@pm.me>
2020-02-16 13:51:49 +0000

    Create snowflake service

---
+++ src/services/SnowflakeService.ts
@@ -0,0 +1,15 @@
+import { Service, Inject } from "typedi";
+import axios from 'axios';
+
+@Service()
+export class SnowflakeService {
+    @Inject('SNOWFLAKE_URL')
+    private readonly url: string;
+
+    @Inject('NODE_ID')
+    private readonly nodeId: string;
+
+    async getSnowflake(): Promise<string> {
+        return axios.get(this.url).then(response => `urn:${this.nodeId}:${response.data.snowflake}`);
+    }
+}
